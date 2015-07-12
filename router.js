b683783cb3060c0b4bcc6e656526d1730f6a4576
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-07-11 21:09:04 -0600

    all CRUD functions working

--- router.js
+++ router.js
@@ -3,9 +3,9 @@ var routes = require('i40') (),
     splatRoutes = require('./routes/splat');
 
 //  CRUD routes
+routes.addRoute('/madlibs/new', madlibRoutes.new)
 routes.addRoute('/', madlibRoutes.index)
 routes.addRoute('/madlibs/:id', madlibRoutes.show)
-routes.addRoute('/madlibs/new', madlibRoutes.new)
 routes.addRoute('/madlibs/:id/edit', madlibRoutes.edit)
 routes.addRoute('/madlibs/:id/update', madlibRoutes.update)
 routes.addRoute('/madlibs/:id/delete', madlibRoutes.destroy)
